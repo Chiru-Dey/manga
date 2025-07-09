d4f39d6726203bdd7a2fde13d27a744762415dab
chiru.d.guy <chiru.d.guy@gmail.com>
2025-07-09 03:12:02 +0000

    remove thumbnail icon

--- Suwayomi-WebUI/src/modules/manga/components/details/Thumbnail.tsx
+++ Suwayomi-WebUI/src/modules/manga/components/details/Thumbnail.tsx
@@ -6,18 +6,13 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import { useState, useLayoutEffect } from 'react';
+import { useTheme } from '@mui/material/styles';
+import { useLayoutEffect, useState } from 'react';
 import Stack from '@mui/material/Stack';
-import MenuItem from '@mui/material/MenuItem';
-import IconButton from '@mui/material/IconButton';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Modal from '@mui/material/Modal';
-import PopupState from 'material-ui-popup-state';
-import { bindTrigger, bindMenu } from 'material-ui-popup-state/hooks';
-import { useLongPress } from 'use-long-press';
+import { bindPopover, bindTrigger, usePopupState } from 'material-ui-popup-state/hooks';
 import { Vibrant } from 'node-vibrant/browser';
 import { FastAverageColor } from 'fast-average-color';
-import { Menu } from '@/modules/core/components/menu/Menu.tsx';
 import { Mangas } from '@/modules/manga/services/Mangas.ts';
 import { SpinnerImage } from '@/modules/core/components/SpinnerImage.tsx';
 import { MANGA_COVER_ASPECT_RATIO } from '@/modules/manga/Manga.constants.ts';
@@ -31,9 +26,12 @@ export const Thumbnail = ({
     manga: Partial<MangaThumbnailInfo>;
     mangaDynamicColorSchemes: boolean;
 }) => {
+    const theme = useTheme();
     const { setDynamicColor } = useAppThemeContext();
-    const [isModalOpen, setIsModalOpen] = useState(false); // State for controlling the modal's open/close status
-    const [isHovered, setIsHovered] = useState(false); // State for controlling hover
+
+    const popupState = usePopupState({ variant: 'popover', popupId: 'manga-thumbnail-fullscreen' });
+
+    const [isImageReady, setIsImageReady] = useState(false);
 
     useLayoutEffect(() => {
         if (!mangaDynamicColorSchemes) {
@@ -82,108 +80,64 @@ export const Thumbnail = ({
     }, []);
 
     return (
-        <PopupState variant="popover" popupId="manga-thumbnail-menu">
-            {(popupState) => {
-                const longPressBind = useLongPress(() => popupState.open(), {
-                    threshold: 500, // Adjust threshold as needed for long press duration
-                    captureEvent: true,
-                });
-                return (
-                    <>
-                        {/* The modal component */}
-                        <Modal
-                            open={isModalOpen}
-                            onClose={() => setIsModalOpen(false)}
-                            sx={{ height: '100vh', p: 2, outline: 0, justifyContent: 'center', alignItems: 'center' }}
-                        >
-                            <SpinnerImage
-                                src={Mangas.getThumbnailUrl(manga)}
-                                alt="Manga Thumbnail"
-                                imgStyle={{ height: '100%', width: '100%', objectFit: 'contain' }}
-                            />
-                        </Modal>
-
-                        {/* Thumbnail Stack */}
-                        <Stack
-                            onMouseEnter={() => setIsHovered(true)}
-                            onMouseLeave={() => setIsHovered(false)}
-                            onClick={() => setIsModalOpen(true)} // Regular click to open modal
-                            {...longPressBind()} // Long press to open menu
-                            sx={{
-                                position: 'relative',
-                                borderRadius: 1,
-                                overflow: 'hidden',
-                                backgroundColor: 'background.paper',
-                                // Removed fixed width for natural scaling
-                                maxHeight: 'fit-content',
-                                aspectRatio: MANGA_COVER_ASPECT_RATIO,
+        <>
+            <Stack
+                sx={{
+                    position: 'relative',
+                    borderRadius: 1,
+                    overflow: 'hidden',
+                    backgroundColor: 'background.paper',
+                    width: '150px',
+                    maxHeight: 'fit-content',
+                    aspectRatio: MANGA_COVER_ASPECT_RATIO,
+                    flexShrink: 0,
+                    flexGrow: 0,
+                    [theme.breakpoints.up('lg')]: {
+                        width: '200px',
+                    },
+                    [theme.breakpoints.up('xl')]: {
+                        width: '300px',
+                    },
+                }}
+            >
+                <SpinnerImage
+                    src={Mangas.getThumbnailUrl(manga)}
+                    alt="Manga Thumbnail"
+                    onLoad={() => setIsImageReady(true)}
+                    imgStyle={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+                {isImageReady && (
+                    <Stack
+                        {...bindTrigger(popupState)}
+                        sx={{
+                            position: 'absolute',
+                            top: 0,
+                            bottom: 0,
+                            width: '100%',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            opacity: 0,
+                            '&:hover': {
+                                background: 'rgba(0, 0, 0, 0.4)',
                                 cursor: 'pointer',
-                            }}
-                        >
-                            <SpinnerImage
-                                src={Mangas.getThumbnailUrl(manga)}
-                                alt="Manga Thumbnail"
-                                imgStyle={{ height: '100%', width: '100%', objectFit: 'contain' }}
-                            />
-                            {isHovered && (
-                                <IconButton
-                                    {...bindTrigger(popupState)}
-                                    size="small"
-                                    sx={{
-                                        position: 'absolute',
-                                        top: 4,
-                                        right: 4,
-                                        zIndex: 1, // Ensure button is above the image
-                                        backgroundColor: 'rgba(0, 0, 0, 0.6)',
-                                        '&:hover': {
-                                            backgroundColor: 'rgba(0, 0, 0, 0.8)',
-                                        },
-                                    }}
-                                >
-                                    <MoreVertIcon fontSize="small" sx={{ color: 'white' }} />
-                                </IconButton>
-                            )}
-                        </Stack>
-
-                        {/* Menu Component */}
-                        <Menu
-                            {...bindMenu(popupState)}
-                            disablePortal
-                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                        >
-                            {(onClose) => (
-                                <>
-                                    <MenuItem
-                                        onClick={() => {
-                                            setIsModalOpen(true); // Open the modal when "Expand" is clicked
-                                            onClose(); // Close the menu
-                                        }}
-                                    >
-                                        Expand
-                                    </MenuItem>
-                                    <MenuItem
-                                        onClick={() => {
-                                            // TODO: Implement Change Thumbnail functionality
-                                            onClose(); // Close the menu
-                                        }}
-                                    >
-                                        Change Thumbnail
-                                    </MenuItem>
-                                    <MenuItem
-                                        onClick={() => {
-                                            // TODO: Implement Reset Thumbnail functionality
-                                            onClose(); // Close the menu
-                                        }}
-                                    >
-                                        Reset Thumbnail
-                                    </MenuItem>
-                                </>
-                            )}
-                        </Menu>
-                    </>
-                );
-            }}
-        </PopupState>
+                                opacity: 1,
+                            },
+                        }}
+                    />
+                )}
+            </Stack>
+            <Modal {...bindPopover(popupState)} sx={{ outline: 0 }}>
+                <Stack
+                    onClick={() => popupState.close()}
+                    sx={{ height: '100vh', p: 2, outline: 0, justifyContent: 'center', alignItems: 'center' }}
+                >
+                    <SpinnerImage
+                        src={Mangas.getThumbnailUrl(manga)}
+                        alt="Manga Thumbnail"
+                        imgStyle={{ height: '100%', width: '100%', objectFit: 'contain' }}
+                    />
+                </Stack>
+            </Modal>
+        </>
     );
 };
