2520ba2e53ce06f0021a8f29208d4d54bb55b0d0
Chiru-Dey <chiru.d.guy@gmail.com>
2025-07-10 05:45:36 +0000

    am 5

--- Suwayomi-WebUI/src/modules/manga/components/ThumbnailOptionButton.tsx
+++ Suwayomi-WebUI/src/modules/manga/components/ThumbnailOptionButton.tsx
@@ -6,36 +6,68 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-import IconButton from '@mui/material/IconButton';
-import { usePopupState, bindTrigger, bindMenu } from 'material-ui-popup-state/hooks';
+import { forwardRef, ForwardedRef } from 'react';
+import Button from '@mui/material/Button';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { useTheme } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
+import { PopupState, bindTrigger } from 'material-ui-popup-state/hooks';
+import { CustomTooltip } from '@/modules/core/components/CustomTooltip.tsx';
+import { MUIUtil } from '@/lib/mui/MUI.util.ts';
 
 export interface ThumbnailOptionButtonProps {
-    popupState: ReturnType<typeof usePopupState>;
+    popupState: PopupState;
 }
 
-export const ThumbnailOptionButton = ({ popupState }: ThumbnailOptionButtonProps) => {
-    const theme = useTheme();
+export const ThumbnailOptionButton = forwardRef(
+    ({ popupState }: ThumbnailOptionButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
+        const { t } = useTranslation();
+        const bindTriggerProps = bindTrigger(popupState);
 
-    return (
-        <IconButton
-            {...bindTrigger(popupState)}
-            {...bindMenu(popupState)}
-            sx={{
-                position: 'absolute',
-                right: 8,
-                top: 8,
-                zIndex: 1,
-                width: 24,
-                height: 48,
-                borderRadius: 2,
-                backgroundColor: theme.palette.background.paper,
-                color: theme.palette.text.primary,
-                '&:hover': { backgroundColor: theme.palette.action.hover },
-            }}
-        >
-            <MoreVertIcon />
-        </IconButton>
-    );
-};
+        const preventDefaultAction = (e: React.BaseSyntheticEvent) => {
+            e.stopPropagation();
+            e.preventDefault();
+        };
+
+        return (
+            <CustomTooltip title={t('global.button.options')}>
+                <Button
+                    ref={ref}
+                    {...MUIUtil.preventRippleProp(bindTriggerProps, { onClick: preventDefaultAction })}
+                    className="manga-option-button"
+                    size="small"
+                    variant="contained"
+                    sx={{
+                        position: 'absolute',
+                        right: 1,
+                        top: 1,
+                        zIndex: 1,
+                        minWidth: 'unset',
+                        paddingX: 0,
+                        paddingY: '2.5px',
+                        visibility: popupState.isOpen ? 'visible' : 'hidden',
+                        pointerEvents: 'none',
+                        '&:hover': {
+                            backgroundColor: (theme) => theme.palette.primary.dark,
+                        },
+                        transition: (theme) => theme.transitions.create(
+                            ['background-color', 'transform', 'visibility'],
+                            { duration: 200 }
+                        ),
+                        '&:active': {
+                            transform: 'scale(0.95)',
+                        },
+                        '@media not (pointer: fine)': {
+                            visibility: 'hidden',
+                            width: 0,
+                            height: 0,
+                            p: 0,
+                            m: 0,
+                        },
+                    }}
+                >
+                    <MoreVertIcon fontSize="small" />
+                </Button>
+            </CustomTooltip>
+        );
+    }
+);
